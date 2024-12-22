f93863ee83e3778c9ba373d41ce06c353e00c7ee
Pax16gamedev <pax16.gamedev@gmail.com>
2024-12-22 22:43:05 +0100

    nuevo fich

---
+++ prueba2 - copia.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    private int ISMA2a;
+
+    void Start()
+    {
+        print("Start");
+        print(1+1)
+        print(1 + 1)
+    }
+}
